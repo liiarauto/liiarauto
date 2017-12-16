d2035c8de8e2b3ad361c7d9c909e5bb75d193397
DSI_sevan <sevanakoumia@gmail.com>
2017-12-16 18:48:54 +0000

    Modification du fichier welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    LiiarAuto
+                    LiiarAuto_ V1
                 </div>
 
                 <div class="links">
