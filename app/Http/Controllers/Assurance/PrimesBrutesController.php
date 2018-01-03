66156bf89b36a22f5d1a4314da26d5319a9fd8cf
Gren Cissé Al-Karid <grencisse@hotmail.com>
2018-01-03 12:38:18 +0100

    Restructuration des données

---
+++ app/Http/Controllers/Assurance/PrimesBrutesController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace LiiarAuto\Http\Controllers\Assurance;
+
+use Illuminate\Http\Request;
+use LiiarAuto\Http\Controllers\Controller as Controller;
+
+use LiiarAuto\Http\Controllers\Assurance\DevisController as DevisController;
+use LiiarAuto\Http\Controllers\Assurance\GarantiesController as GarantiesController;
+
+class PrimesBrutesController extends Controller
+{
+    protected $devis;
+
+    public function __construct()
+    {
+        $this->devis = new DevisController();
+        $this->garanties = new GarantiesController();
+    }
+
+    /**
+     * @param Categorie
+     * @param Puissance
+     * @param Energie
+     * @param ChargeUtile
+     * @param nbPersonnes
+     * @param Zone
+     */
+    public function responsabiliteCivile($categorie, $puissance, $energie, $chargeUtile, $nbPersonnes, $zone)
+    {
+        return $this->devis->rcCat01($categorie, $puissance, $energie, $chargeUtile, $nbPersonnes, $zone);
+    }
+
+    public function defenseEtRecours($categorie, $rcCat01=null, $fractionPrime=null)
+    {
+        return $this->garanties->defenseEtRecours($categorie, $rcCat01, $fractionPrime);
+    }
+
+    public function incendie($categorie, $sousCategorie=1, $valeurVenale)
+    {
+        return $this->garanties->incendie($categorie, $sousCategorie, $valeurVenale);
+    }
+
+    public function volVolAgressionEtVolAccessoires($categorie, $valeurVenale)
+    {
+        return $this->garanties->volVolAgressionEtVolAccessoires($categorie, $valeurVenale);
+    }
+
+    public function brisDeGlace($categorie, $valeurNeuve)
+    {
+        return $this->garanties->brisDeGlace($categorie, $valeurNeuve);
+    }
+
+    public function indPersonnesTransportees($nbPlace, $formule=1)
+    {
+        return $this->garanties->indPersonnesTransportees($nbPlace, $formule);
+    }
+}
---
+++ app/Http/Controllers/Assurance/Reductions/ReductionsReductionController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace LiiarAuto\Http\Controllers\Assurance\Reductions;
+
+use Illuminate\Http\Request;
+
+class PermisReductionController extends Controller
+{
+    //
+}
