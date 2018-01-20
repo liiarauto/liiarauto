df7030cb91afe76a94b4a1001eb5f7a648121fd4
cyrilleIT <cyrilleable@gmail.com>
2018-01-20 19:23:01 +0000

    Premier commit de refactor

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# liiarauto
+LIIARAUTO
+
+* Valeus de test
+    * EFFET	18/12/2016			
+    * ECHEANCE 17/06/2017
+    * Catégorie 1
+    * Puissance 14
+    * Energie E
+    * Places Assises 5
+    * Date de mise en circulation 15/02/2007
+    * Zone de circulation 1
+    * Valeur neuve 20000000
+    * Valeur vénale 6000000
---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,36 @@
+<?php return array (
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laravelcollective/html' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Collective\\Html\\HtmlServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Form' => 'Collective\\Html\\FormFacade',
+      'Html' => 'Collective\\Html\\HtmlFacade',
+    ),
+  ),
+  'xethron/migrations-generator' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Way\\Generators\\GeneratorsServiceProvider',
+      1 => 'Xethron\\MigrationsGenerator\\MigrationsGeneratorServiceProvider',
+    ),
+  ),
+);
